563bbc29b61514db329f51328608bfcb2742f625
strazzullociroandrea <135723728+strazzullociroandrea@users.noreply.github.com>
2024-04-06 16:26:36 +0000

    ultime sistemazioni

--- public/src/controller/index.js
+++ public/src/controller/index.js
@@ -10,38 +10,4 @@ invia.onclick = () => {
     esponente.style.borderColor = "";
     visualizza(esponente.value);
   }
-  /*
-  fetch("/calcola", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      esponente: esponente.value,
-    }),
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      esponente.value = "";
-      // Generazione view risultato
-      let data = response.result.newton;
-      risultato.innerHTML = data.replace(/\^(\d+)/g, "<sup>$1</sup>");
-
-      // Generazione view tartaglia
-      const tartaglia = response.result.tartaglia;
-      let tartagliaHTML = "";
-      tartaglia.forEach((row) => {
-        tartagliaHTML += "<div><p>";
-        row.forEach((element,index) => {
-          if(index < row.length-1)
-            tartagliaHTML +=  element + " ";
-          else 
-            tartagliaHTML +=  element 
-        });
-        tartagliaHTML += "</p></div>";
-      });
-      tartagliaOutput.innerHTML = tartagliaHTML;
-    })
-    .catch((error) => console.error("Errore durante la richiesta:", error));
-     */
 };
