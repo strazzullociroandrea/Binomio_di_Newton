b81c72eec1a5501edec3555798746badf0883c6d
Mirko <148048359+plettroexe@users.noreply.github.com>
2024-04-08 06:59:50 +0000

    correzione test

--- test/test.test.js
+++ test/test.test.js
@@ -1,28 +1,27 @@
-import { expect } from 'chai';
-import { createRequire } from 'module';
+import { expect } from "chai";
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const { calcoloCoefficiente } = require('../binomioNewton.cjs');
+const { calcoloCoefficiente } = require("../binomioNewton.cjs");
 
 describe("Testing binomio di Newton e triangolo di Tartaglia", function () {
-    it("1. Calcolo del coefficiente di 5 su 3 ", async function () {
-        expect(await calcoloCoefficiente(5, 3)).to.equal(19);//test errato
-    });
+  it("1. Calcolo del coefficiente di 5 su 3 ", async function () {
+    expect(await calcoloCoefficiente(5, 3)).to.equal(19); //test errato
+  });
 });
 
-import { calcoloGenerale } from '../binomioNewton.cjs';
+import { calcoloGenerale } from "../binomioNewton.cjs";
 
 describe("Testing calcoloGenerale function", function () {
-    it("should calculate correctly for given exponent", async function () {
-        const expectedResults = {
-            0: "a^3 + 3ab + b^3",
-            1: "a^2 + 2ab + b^2",
-            2: "a + b"
-
-        };
-        for (const exponent of Object.keys(expectedResults)) {
-            const expectedResult = expectedResults[exponent];
-            const result = await calcoloGenerale(exponent);
-            expect(result).to.equal(expectedResult);
-        }
-    });
+  it("should calculate correctly for given exponent", async function () {
+    const expectedResults = {
+      3: "a^3 + 3ab + b^3",
+      2: "a^2 + 2ab + b^2",
+      1: "a + b",
+    };
+    for (const exponent of Object.keys(expectedResults)) {
+      const expectedResult = expectedResults[exponent];
+      const result = await calcoloGenerale(exponent);
+      expect(result).to.equal(expectedResult);
+    }
+  });
 });
